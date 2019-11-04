17f0791aba5156f0d3536fdc82044335be8dbda0
kn <kn@kns-MacBook-Pro.local>
2019-11-04 18:51:09 +0700

    script to build

---
+++ labwork/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if ls build* 1> /dev/null 2>&1; then
+  rm -rf build &&
+  echo "Building..."
+  mkdir build &&
+  cd build &&
+  cmake .. &&
+  make -j
+else
+  echo "Building..." &&
+  mkdir build &&
+  cd build &&
+  cmake .. &&
+  make -j &&
+fi
